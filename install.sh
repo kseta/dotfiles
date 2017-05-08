3c607b9e981c41c5b1752ad29925e4f3981a6db0
kseta <keigo.seta@gmail.com>
2017-05-08 15:08:56 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+ #!/bin/sh
+
+ set -e
+ set -u
+
+ setup() {
+     dotfiles=$HOME/.ghq/github.com/kseta/dotfiles
+
+     has() {
+         type "$1" > /dev/null 2>&1
+     }
+
+     symlink() {
+         [ -e "$2" ] || ln -s "$1" "$2"
+     }
+
+     if [ -d "$dotfiles" ]; then
+         (cd "$dotfiles" && git pull --rebase)
+     else
+         git clone https://github.com/kseta/dotfiles "$dotfiles"
+     fi
+
+     has git && symlink "$dotfiles/home/.gitconfig" "$HOME/.gitconfig"
+     has git && symlink "$dotfiles/home/.zshrc" "$HOME/.zshrc"
+     has vim && symlink "$dotfiles/home/.vimrc" "$HOME/.vimrc"
+ }
+
+ setup
